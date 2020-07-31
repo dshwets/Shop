6d76a00714a005e8c30983e98e49324b26ed77b6
Daniil <dshwets@gmail.com>
2020-07-31 15:21:57 +0600

    Сделал URL и формы. За основу взял прошлые работы :)

--- source/main/urls.py
+++ source/main/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import index_view, watch_product, create_product, update_product, delete_product
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
+    path('product/<int:pk>/', watch_product, name='watch_product'),
+    path('product/add/', create_product, name='create_product'),
+    path('product/<int:pk>/update/', update_product, name='update_product'),
+    path('product/<int:pk>/delete/', delete_product, name='product_delete'),
 ]
---
+++ source/webapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import CATEGORY_CHOICES, DEFAULT_CATEGORY
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class ProductForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, label='Название')
+    description = forms.CharField(max_length=2000, required=False, label='Описание', widget=forms.Textarea)
+    category = forms.ChoiceField(choices=CATEGORY_CHOICES, required=True, initial=DEFAULT_CATEGORY,
+                                 label='Категория')
+    amount = forms.IntegerField(min_value=0, required=True, label="Остаток")
+    price = forms.DecimalField(min_value=0, max_digits=7, decimal_places=2, required=True, label='Цена')
